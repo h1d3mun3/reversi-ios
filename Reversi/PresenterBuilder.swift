99726ee5d7a2986d60dae4181a5e2cf061f72b7b
Hidemune Takahashi <h1d3mun3.74k4h45h1@gmail.com>
2020-11-06 16:38:00 +0900

    Fix Build Error.

--- Reversi/PresenterBuilder.swift
+++ Reversi/PresenterBuilder.swift
@@ -5,10 +5,10 @@ struct PresenterBuilder {
         let countDiskUseCase = CountDiskUseCase(loadGameUseCase: LoadGameUseCase())
         let getAllPossibleCoordinatesByDiskUseCase = GetAllPossibleCoordinatesByDiskUseCase(
             loadGameUseCase: LoadGameUseCase(),
-            getAllCoordinatesAffectedUseCase: GetAllCoordinatesAffectedUseCase(getDiskFromBoardUseCase: GetDiskFromBoardUseCase(loadGameUseCase: LoadGameUseCase())))
+            getAllCoordinatesAffectedUseCase: GetAllCoordinatesAffectedUseCase(loadGameUseCase: LoadGameUseCase(), getDiskFromBoardUseCase: GetDiskFromBoardUseCase(loadGameUseCase: LoadGameUseCase())))
         let placeDiskUseCase = PlaceDiskUseCase(loadGameUseCase: LoadGameUseCase(),
                                                 getAllCoordinatesAffectedUseCase: GetAllCoordinatesAffectedUseCase(
-                                                    getDiskFromBoardUseCase: GetDiskFromBoardUseCase(loadGameUseCase: LoadGameUseCase()
+                                                    loadGameUseCase: LoadGameUseCase(), getDiskFromBoardUseCase: GetDiskFromBoardUseCase(loadGameUseCase: LoadGameUseCase()
                                                 ))
         )
         let presenter = Presenter(view: view, saveGameUseCase: saveGameUseCase, loadGameUseCase: loadGameUseCase, countDiskUseCase: countDiskUseCase, getAllPossibleCoordinatesByDiskUseCase: getAllPossibleCoordinatesByDiskUseCase, placeDiskUseCase: placeDiskUseCase)
